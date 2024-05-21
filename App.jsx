44fbb2dbd44652a8789a51e785e38917b4284749
khushiiiiiiioooooooh <khushibagda2207@gmail.com>
2024-05-21 13:07:37 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,60 +1,19 @@
 import { useState } from 'react';
 
-function CounterApp() {
-  const [count, setCount] = useState(10); // Assuming the initial count is 10 as per the image
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const styles = {
-    body: {
-      display: 'flex',
-      alignItem:'center',
-      justifyContent:'center',
-    },
-    container: {
-      textAlign: 'center',
-      fontFamily: 'Arial',
-      marginTop: '50px',
-      margin:'25%',
-    },
-    title: {
-      fontSize: '124px',
-      color: '#fff'
-    },
-    countDisplay: {
-      fontSize: '58px',
-      margin: '20px 0'
-    },
-    button: {
-      width: '50px',
-      height: '50px',
-      borderRadius: '50%',
-      
-      border: 'none',
-      fontSize: '15px',
-      margin: '0 10px',
-      cursor: 'pointer',
-      alignSelf: 'center',
-      justifySelf:'center'
-      },
-    increaseButton: {
-      backgroundColor: 'green'
-    },
-    decreaseButton: {
-      backgroundColor: 'red'
-    },
-    resetButton: {
-      backgroundColor: 'aqua'
-    }
+  const handleButtonClick = () => {
+    setIsDarkMode(!isDarkMode);
   };
 
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>Counter App</h1>
-      <div style={styles.countDisplay}>{count}</div>
-      <button style={{ ...styles.button, ...styles.increaseButton }} onClick={() => setCount(count + 1)}>+</button>
-      <button style={{ ...styles.button, ...styles.decreaseButton }} onClick={() => setCount(count - 1)}>-</button>
-      <button style={{ ...styles.button, ...styles.resetButton }} onClick={() => setCount(0)}>0</button>
+    <div style={{ backgroundColor: isDarkMode ? 'black' : 'white', height: '100vh',width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <button onClick={handleButtonClick} style={{ color: isDarkMode ? 'white' : 'black', backgroundColor: 'transparent',border: isDarkMode ? '2px solid white' : '2px solid black' }}>
+        {isDarkMode ? 'White' : 'Dark'}
+      </button>
     </div>
   );
-}
+};
 
-export default CounterApp;
+export default App;
